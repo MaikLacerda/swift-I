7e197ae49fa8dd37b7156cafd44da2796ae4e634
Maikon Lacerda <maiklacerdaa@gmail.com>
2019-09-20 16:17:45 -0300

    metodo classe  oo

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -12,3 +12,36 @@ func allCalories(calories:Array<Double>) -> Double {
 }
 
 allCalories(calories: [10.5, 100, 300, 500, 300, 300])
+
+
+//var values = [1,2]
+//var total = 0
+//for v in values{
+//    total = v
+//}
+//print(total / values.count)
+
+//calculo soma com arrays
+func calculaSoma(valores:Array<Double>) -> Double {
+    var soma:Double = 0
+    for valor in valores {
+        soma += valor
+    }
+    return soma
+}
+// trabalhando com o metodo classe
+class Meal {
+    var happiness:Int = 5
+    var name:String = "eggplant brownie"
+}
+
+var brownie = Meal()
+print(brownie.happiness)
+print(brownie.name)
+
+var hamburguer = Meal()
+hamburguer.happiness = 4
+hamburguer.name = "Hamburguer"
+print(hamburguer.happiness)
+print(hamburguer.name)
+
